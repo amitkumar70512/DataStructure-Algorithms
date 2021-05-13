e55a9ae697de919bb044a305abe559f803d90eba
amit kumar <71318008+amitkumar70512@users.noreply.github.com>
2021-05-13 21:45:48 +0530

    Add files via upload

---
+++ BinarySearch_with repeated integers(first and last occcurence)/main.c
@@ -0,0 +1,87 @@
+///  2nd one
+#include<stdio.h>
+#include<stdbool.h>
+#include<time.h>
+
+clock_t start, end;
+    double cpu_time_used;
+
+
+int binarysearch(int a[],int n,int k,bool searchfirst){
+    start = clock();
+    for(int i=0;i<80000000;i++);
+    int result=-1;
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(a[mid]==k)  {
+              result=mid;
+           if(searchfirst)
+              high=mid-1;
+            else
+              low=mid+1;
+    }
+    else if(k<a[mid])  high=mid-1;
+    else low=mid+1;
+    }
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    return result;
+}
+void sorting(int a[],int n)
+{
+    int i,j,temp;
+    for (i = 1 ; i <= n - 1; i++)
+    {
+	    j = i;
+            while ( j > 0 && a[j-1] > a[j])
+            {
+                temp     = a[j];
+                a[j]   = a[j-1];
+                a[j-1] = temp;
+                j--;
+            }
+    }
+}
+void display(int a[],int size)
+    {
+         printf("\n \n \t Displaying entered array.. ");
+        for(int k=0;k<size;k++)
+        {
+            printf(" %d ",a[k]);
+        }
+    }
+int main(){
+    int size,key;
+    printf("\n Enter the number of elements for an array:> \t");
+    scanf("%d",&size);
+    int a[size];
+    printf("\n enter the %d elements in the array.\n",size);
+    for(int i=0;i<size;i++)
+    {
+        printf("\nelement %d:- ",i+1);
+        scanf("%d",&a[i]);
+
+    }
+    display(a,size);
+
+     sorting(a,size);
+     printf("\n \t After sorting array ..");
+     display(a,size);
+    int n=sizeof(a)/sizeof(a[0]);
+    int x;
+    printf("\n Enter the  key  element:\t");
+    scanf("%d",&x);
+    int f=binarysearch(a,n,x,true);
+    printf("first occurence at %d index \n",f);
+    if(f==-1){
+        printf("elment not found in the array:\n ");
+    }
+    else {
+        int l=binarysearch(a,n,x,false);
+        printf("last occurence at %d index\n",l);
+        printf("count is = %d", l-f+1);
+    }
+  printf("\n\t Time complexity, O(Logn) In case of recursive Binary implementation\n");
+  printf("It took %f seconds to execute..\n ",cpu_time_used);
+}
